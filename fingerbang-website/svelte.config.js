c00e3456d5240c520e041c0ecb3131ab6c620c40
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2024-09-16 13:47:21 +0300

    update node

--- fingerbang-website/svelte.config.js
+++ fingerbang-website/svelte.config.js
@@ -19,11 +19,6 @@ import node from '@sveltejs/adapter-node';
 const config = {
 	kit: {
 		adapter: node(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
 	}
 };
 
